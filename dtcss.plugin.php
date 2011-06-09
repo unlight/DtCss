57f616ea372a5ee713f03f926b1295980aff5d95
S <unlights@gmail.com>
2011-06-09 11:45:38 +0400

    Disabled auto clean cache, icon, .gitignore updated

--- dtcss.plugin.php
+++ dtcss.plugin.php
@@ -2,15 +2,17 @@
 
 $PluginInfo['DtCss'] = array(
 	'Name' => 'DtCss',
-	'Description' => 'Adapts DtCSS to work with Garden.',
-	'Version' => '1.2.5',
+	'Description' => 'Adapts DtCSS to work with Garden. DtCSS is a PHP script that preprocesses your CSS file. It speeds up CSS coding by extending the features to CSS. Such as nested selectors, color mixing and more. DtCSS reads the CSS file with special syntax written for DtCSS, and outputs the standard CSS. <a href="http://code.google.com/p/dtcss/">DtCSS project home page</a>',
+	'Version' => '1.3f',
 	'Author' => 'DtCss',
-	'AuthorUrl' => 'http://code.google.com/p/dtcss/',
+	'Date' => 'Summer 2011',
+	'AuthorUrl' => 'http://github.com/search?q=DtCss&type=Repositories',
 	'RequiredApplications' => False,
 	'RequiredTheme' => False,
 	'RequiredPlugins' => False,
 	'RegisterPermissions' => False,
-	'SettingsPermission' => False
+	'SettingsPermission' => False,
+	'License' => 'X.Net License'
 );
 
 /*
@@ -21,7 +23,7 @@ TODO:
 
 class DtCssPlugin extends Gdn_Plugin {
 	
-	public function Tick_Every_20_Days_Handler() {
+/*	public function Tick_Every_20_Days_Handler() {
 		self::_EmptyCache();
 	}
 	
@@ -30,10 +32,10 @@ class DtCssPlugin extends Gdn_Plugin {
 	}
 	
 	public function SettingsController_AfterDisablePlugin_Handler() { 
-		self::_EmptyCache(); 
-	}
+		self::_EmptyCache();
+	}*/
 	
-	private static function _EmptyCache() {
+	public static function _EmptyCache() {
 		$DirectoryAry = array(PATH_APPLICATIONS, PATH_PLUGINS, PATH_THEMES);
 		foreach($DirectoryAry as $DirectoryPath) {
 			$Directory = new RecursiveDirectoryIterator($DirectoryPath);
@@ -42,7 +44,7 @@ class DtCssPlugin extends Gdn_Plugin {
 				$Extension = pathinfo($Basename, 4);
 				$Filename = pathinfo($Basename, 8);
 				if ($Extension != 'css') continue;
-				if (!preg_match('/^[\.\w]+\-c\-[a-z0-9]{6}$/', $Filename)) continue;
+				if (!preg_match('/^[\.\w\-]+\-c\-[a-z0-9]{5,7}$/', $Filename)) continue;
 				$CachedFile = $File->GetRealPath();
 				unlink($CachedFile);
 			}
@@ -56,7 +58,7 @@ class DtCssPlugin extends Gdn_Plugin {
 	}
 	
 	
-	public static function GetHash($S) {
+	protected static function GetHash($S) {
 		$Crc = sprintf('%u', crc32($S));
 		$Hash = base_convert($Crc, 10, 36);
 		$Hash = sprintf("%06s", substr($Hash, -6)); // zero-padding for string length < 6
@@ -64,7 +66,7 @@ class DtCssPlugin extends Gdn_Plugin {
 	}
 	
 	public static function MakeCssFile($CssPath, $CachedCssFile) {
-		if (!function_exists('DtCSS')) require dirname(__FILE__).DS.'DtCSS-R27f'.DS.'libdtcss.php';
+		if (!function_exists('DtCSS')) require dirname(__FILE__) . '/DtCSS-R27f/libdtcss.php';
 		$Filedata = file_get_contents($CssPath);
 		// $CssPath need for #include directives, will use dirname($CssPath)/other.css
 		$Data = DtCSS($CssPath, $Filedata);
@@ -98,15 +100,17 @@ class DtCssPlugin extends Gdn_Plugin {
 			}
 			
 			if($CssPath == False) continue; // not found
-			
+
 			$Basename = pathinfo(pathinfo($CssPath, 8), 8); // without .dt.css
 			$Hash = self::GetHash($CssPath . filemtime($CssPath));
 			
-			$CacheFileName = sprintf('.%s-c-%s.css', $Basename, $Hash);
+			$CacheFileName = sprintf('__%s-c-%s.css', $Basename, $Hash);
 			$CachedCssFile = dirname($CssPath).DS.$CacheFileName;
 			if (!file_exists($CachedCssFile)){
 				self::MakeCssFile($CssPath, $CachedCssFile, True);
 			}
+			
+			// TODO: use AbsoluteSource() from 2.0.18
 	
 			// ... and replace preprocessored dt.css file by css
 			$CssInfo['FileName'] = substr($CachedCssFile, strlen(PATH_ROOT));
